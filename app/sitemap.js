a4b15a5ea3487c184ed146b2bf41f1f05ae4a3fd
meyssamh <meyssamh@web.de>
2025-03-21 14:54:32 +0330

    improved seo

---
+++ app/sitemap.js
@@ -0,0 +1,17 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://meyssamdev.ir',
+            lastModified: new Date(),
+            alternates: {
+                languages: {
+                    de: 'https://meyssamdev.ir/de',
+                    en: 'https://meyssamdev.ir/en',
+                    es: 'https://meyssamdev.ir/es',
+                    fa: 'https://meyssamdev.ir/fa',
+                    fr: 'https://meyssamdev.ir/fr',
+                },
+            },
+        },
+    ]
+}
