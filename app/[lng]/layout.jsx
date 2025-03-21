e8a481027ec4c1ee1137e1e9d75d5d567d4e3035
meyssamh <meyssamh@web.de>
2025-03-21 16:21:22 +0330

    fixed minor bug

--- app/[lng]/layout.jsx
+++ app/[lng]/layout.jsx
@@ -9,9 +9,35 @@ export async function generateStaticParams() {
 };
 
 export const metadata = {
-	title: 'Meyssam Hosseinifard',
-	description: 'Portfolio website of Seyed Meyssam Hosseinifard',
+	title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
+	description: 'Web developer and junior Java developer specializing in Next.js, React, Java, and modern web technologies. Explore my projects, blog, and contact me for collaboration.',
+	keywords: 'Web Developer, Next.js Developer, React Developer, Junior Java Developer, Frontend Engineer, Software Engineer, JavaScript, Java, Portfolio, Meyssam Hosseinifard',
+	author: 'Seyed Meyssam Hosseinifard',
+	openGraph: {
+		title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
+		description: 'Explore the portfolio of Seyed Meyssam Hosseinifard, a skilled Next.js, React developer, and junior Java developer. See my latest projects and contact me for collaboration.',
+		url: 'https://meyssamdev.ir',
+		siteName: 'Meyssam Hosseinifard Portfolio',
+		images: [
+			{
+				url: 'https://meyssamdev.ir/og-image.jpg',
+				width: 1200,
+				height: 630,
+				alt: 'Meyssam Hosseinifard Portfolio',
+			},
+		],
+		type: 'website',
+	},
+	// twitter: {
+	// 	card: 'summary_large_image',
+	// 	site: '@yourtwitterhandle',
+	// 	title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
+	// 	description: 'Discover my projects in Next.js, React, and Java development. Contact me for collaboration!',
+	// 	images: ['https://meyssamdev.ir/og-image.jpg'],
+	// },
 };
+
+
 
 export default function RootLayout({
 	children,
