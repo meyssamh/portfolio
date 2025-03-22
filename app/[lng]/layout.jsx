f862479080781fb84e0b511d680d4f5994b70f41
meyssamh <meyssamh@web.de>
2025-03-22 12:45:31 +0330

    made seo better

--- app/[lng]/layout.jsx
+++ app/[lng]/layout.jsx
@@ -10,7 +10,7 @@ export async function generateStaticParams() {
 
 export const metadata = {
 	title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
-	description: 'Web developer and junior Java developer specializing in Next.js, React, Java, and modern web technologies. Explore my projects, blog, and contact me for collaboration.',
+	description: "Seyed Meyssam Hosseinifard is a skilled web developer and junior Java developer specializing in Next.js, React, and Java. Explore his portfolio, learn about modern web technologies, and discover high-performance software engineering solutions.",
 	keywords: 'Web Developer, Next.js Developer, React Developer, Junior Java Developer, Frontend Engineer, Software Engineer, JavaScript, Java, Portfolio, Meyssam Hosseinifard',
 	author: 'Seyed Meyssam Hosseinifard',
 	openGraph: {
@@ -20,21 +20,50 @@ export const metadata = {
 		siteName: 'Meyssam Hosseinifard Portfolio',
 		images: [
 			{
-				url: 'https://meyssamdev.ir/og-image.jpg',
+				url: '/og-image.jpg',
 				width: 1200,
 				height: 630,
 				alt: 'Meyssam Hosseinifard Portfolio',
 			},
 		],
 		type: 'website',
+	}, other: {
+		'application/ld+json': JSON.stringify({
+			"@context": "https://schema.org",
+			"@type": "Person",
+			"name": "Seyed Meyssam Hosseinifard",
+			"url": "https://meyssamdev.ir",
+			"image": "https://meyssamdev.ir/og-image.jpg",
+			"jobTitle": "Web Developer & Junior Java Developer",
+			"description": "Web developer and junior Java developer specializing in Next.js, React, Java, and modern web technologies.",
+			"sameAs": [
+				"https://github.com/meyssamh",
+				"https://www.linkedin.com/in/seyed-meyssam-h-b97318166",
+				"https://x.com/JungeAmadeus?t=s8knmGOJkGRoyK8YBYvckg&s=08"
+			],
+			"worksFor": {
+				"@type": "Organization",
+				"name": "Freelance"
+			},
+			"alumniOf": {
+				"@type": "CollegeOrUniversity",
+				"name": "Applied Scientific University of Industries of Iran",
+				"sameAs": "https://msi-uast.ir"
+			}
+		})
 	},
-	// twitter: {
-	// 	card: 'summary_large_image',
-	// 	site: '@yourtwitterhandle',
-	// 	title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
-	// 	description: 'Discover my projects in Next.js, React, and Java development. Contact me for collaboration!',
-	// 	images: ['https://meyssamdev.ir/og-image.jpg'],
-	// },
+	twitter: {
+		card: 'summary_large_image',
+		site: '@JungeAmadeus',
+		title: 'Seyed Meyssam Hosseinifard | Web Developer & Junior Java Developer',
+		description: 'Discover my projects in Next.js, React, and Java development. Contact me for collaboration!',
+		images: ['/og-image.jpg'],
+	},
+	icons: {
+		icon: '/favicon.ico',
+		shortcut: '/favicon.ico',
+		apple: '/favicon-32x32.png',
+	},
 };
 
 
